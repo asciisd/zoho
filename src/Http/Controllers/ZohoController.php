6cad80bd7f8e5e231cb2e106d3eec9b5a1b43b15
Amr Ahmed <aemaddin@gmail.com>
2020-05-26 03:30:26 +0200

    fixes ZohoController ZCRMRestClient::initialize(Zoho::zohoOptions()) missing

--- src/Http/Controllers/ZohoController.php
+++ src/Http/Controllers/ZohoController.php
@@ -4,12 +4,15 @@ namespace Asciisd\Zoho\Http\Controllers;
 
 use Asciisd\Zoho\Http\Requests\ZohoRedirectRequest;
 use Illuminate\Routing\Controller;
+use zcrmsdk\crm\setup\restclient\ZCRMRestClient;
 use zcrmsdk\oauth\ZohoOAuth;
+use Zoho;
 
 class ZohoController extends Controller
 {
     public function oauth2callback(ZohoRedirectRequest $request)
     {
+        ZCRMRestClient::initialize(Zoho::zohoOptions());
         $oAuthClient = ZohoOAuth::getClientInstance();
         $$oAuthClient->generateAccessToken($request->code);
 
